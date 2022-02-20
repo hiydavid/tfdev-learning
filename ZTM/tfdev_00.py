280f3eddd9d34a75b1389449d8166268504b08b8
David Huang <42119351+hiydavid@users.noreply.github.com>
2022-02-19 21:48:24 -0800

    Update tfdev_00.py

--- ZTM/tfdev_00.py
+++ ZTM/tfdev_00.py
@@ -42,16 +42,15 @@ shape, rank, and size of tensor: {tensor.shape, tensor.ndim, tf.size(tensor)}
 
 
 # 3. Create two tensors containing random values between 0 and 1 with shape [5, 300].
-random_tensor_shape = [5, 300]
-random_tensor_1 = tf.random.uniform(
-    shape=random_tensor_shape,
+A = tf.random.uniform(
+    shape=[5, 300],
     minval=0,
     maxval=1,
     dtype=tf.float32,
     seed=420
 )
-random_tensor_2 = tf.random.uniform(
-    shape=random_tensor_shape,
+B = tf.random.uniform(
+    shape=[5, 300],
     minval=0,
     maxval=1,
     dtype=tf.float32,
@@ -60,21 +59,68 @@ random_tensor_2 = tf.random.uniform(
 
 print(f"""
 ========== Q3 ==========
-random tensor #1: {random_tensor_1}
-random tensor #2: {random_tensor_2}
+random tensor #1: {A}
+random tensor #2: {B}
 """)
 
 
-# 4.
+# 4. Multiply the two tensors you created in 3 using matrix multiplication.
+print(f"""
+========== Q4 ==========
+{A @ tf.transpose(B)}
+""")
 
-# 5.
 
-# 6.
+# 5. Multiply the two tensors you created in 3 using dot product.
+print(f"""
+========== Q5 ==========
+{tf.tensordot(A, tf.transpose(B), axes=1)}
+""")
 
-# 7.
 
-# 8.
+# 6. Create a tensor with random values between 0 and 1 with shape [224, 224, 3].
+C = tf.random.normal([224, 224, 3])
 
-# 9.
+print(f"""
+========== Q6 ==========
+{C}
+""")
 
-# 10.
+# 7. Find the min and max values of the tensor you created in 6 along the first axis.
+C_min_1axis = tf.math.reduce_min(C)
+C_max_1axis = tf.math.reduce_max(C)
+
+print(f"""
+========== Q7 ==========
+min value of first axis: {C_min_1axis}\n
+max value of first axis: {C_max_1axis}\n
+""")
+
+# 8. Created a tensor with random values of shape [1, 224, 224, 3] then squeeze it to change the shape to [224, 224, 3].
+D = tf.random.normal([1, 224, 224, 3])
+D_squeezed = tf.squeeze(D)
+
+print(f"""
+========== Q8 ==========
+original tensor shape: {D.shape}
+squeezed tensor shape: {D_squeezed.shape}
+""")
+
+# 9. Create a tensor with shape [10] using your own choice of values, then find the index which has the maximum value.
+E = tf.random.normal([10])
+
+print(f"""
+========== Q9 ==========
+tensor's values: {E}
+tensor's max value: {tf.reduce_max(E)}
+tensor's max value's position: {tf.argmax(E)}
+""")
+
+# 10. One-hot encode the tensor you created in 9.
+print(f"""
+========== Q10 ==========
+original tensor: {E}
+one-hot encoded tensor: {tf.one_hot(
+    indices=tf.cast(E, dtype=tf.int32).numpy(),
+    depth=tf.size(E).numpy())}
+""")
